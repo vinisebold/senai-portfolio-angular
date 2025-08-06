e97801e1ed26412951f8654578fb41c8fcbe7a81
Vinicius Sebold <63889194+vinisebold@users.noreply.github.com>
2025-08-06 12:01:50 +0000

    feat: adiciona estrutura de componentes

---
+++ senai-portfolio-angular/src/app/features/ciencias-natureza/tri-1/tri-1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tri-1',
+  imports: [],
+  templateUrl: './tri-1.html',
+  styleUrl: './tri-1.scss'
+})
+export class Tri1 {
+
+}
---
+++ senai-portfolio-angular/src/app/features/linguagens/tri-2/tri-2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tri-2',
+  imports: [],
+  templateUrl: './tri-2.html',
+  styleUrl: './tri-2.scss'
+})
+export class Tri2 {
+
+}
